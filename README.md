f147b936680ac83580fdb2b3c41862b73a0cc2dc
Muddassar Shaikh <muddassarshaikh@winjit.com>
2018-12-31 11:21:19 +0530

    Change in README file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # commonAPI
 Common API used by each and every project
+
+POSTMAN Link for API -- https://www.getpostman.com/collections/f1b49d0f6d70e8574bc4
--- modules/common/authentication.js
+++ modules/common/authentication.js
@@ -3,61 +3,49 @@ const code = require('./code');
 const message = require('./message');
 
 const authenticationController = {
-    
+
     validateToken: (req, res, next) => {
-        try
-        {
-            if(req.headers.auth)
-            {          
+        try {
+            if (req.headers.auth) {
                 functions.tokenDecrypt(req.headers.auth, (err, result) => {
-                    if(result)
-                    {
-                        if(result.data)
-                        {
+                    if (result) {
+                        if (result.data) {
                             res.locals.id = result.data.id;
                             const token = functions.tokenEncrypt(result.data);
                             res.header('auth', token);
                             next();
                         }
-                        else
-                        {
+                        else {
                             res.send(functions.responseGenerator(code.invalidDetails, message.tokenIssue));
                         }
                     }
-                    else
-                    {
+                    else {
                         res.send(functions.responseGenerator(code.sessionExpire, message.sessionExpire));
                     }
                 });
             }
-            else
-            {
+            else {
                 res.send(functions.responseGenerator(code.invalidDetails, message.tokenMissing));
             }
         }
-        catch(e)
-        {
+        catch (e) {
             console.log(e);
             res.send(functions.responseGenerator(code.invalidDetails, message.tryCatch, e));
         }
     },
-    
+
     decryptRequest: (req, res, next) => {
-        try
-        {
-            if(req.body.encRequest)
-            {
-                const userinfo  = functions.decryptData(req.body.encRequest);
+        try {
+            if (req.body.encRequest) {
+                const userinfo = functions.decryptData(req.body.encRequest);
                 res.locals.data = userinfo;
                 next();
             }
-            else
-            {
+            else {
                 res.send(functions.responseGenerator(code.invalidDetails, message.dataIssue));
             }
         }
-        catch(e)
-        {
+        catch (e) {
             console.log(e);
             res.send(functions.responseGenerator(code.invalidDetails, message.tryCatch, e));
         }
