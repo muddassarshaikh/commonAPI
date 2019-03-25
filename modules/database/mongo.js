e53b9eb610d22fb90ccd67065f1f1a31e9a06ebb
Muddassar Shaikh <shaikh.muddassar8@gmail.com>
2019-03-25 11:24:50 +0530

    Added comment

--- modules/database/mongo.js
+++ modules/database/mongo.js
@@ -1,35 +1,37 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
+// MongoDB Database file
+
+const mongoose = require("mongoose");
 
 //constiable to set connection string
-const mongoDBConnectinString = 'mongodb://localhost:27017/common';
+const mongoDBConnectinString = "mongodb://localhost:27017/common";
 
-const host = 'localhost:1337';
+const host = "localhost:1337";
 
-const secretKey = 'a4f90771f-c973-4947-86e2';
+const secretKey = "a4f90771f-c973-4947-86e2";
 
 /*create connection for the first time*/
 const db = mongoose.createConnection(mongoDBConnectinString);
 
 mongoose.connect(mongoDBConnectinString);
 
-db.on('error', function (err) {
-    console.log('Mongoose connection error: ' + err);
+db.on("error", function(err) {
+  console.log("Mongoose connection error: " + err);
 });
 
-db.on('connected', function () {
-    console.log('Mongoose connected ');
+db.on("connected", function() {
+  console.log("Mongoose connected ");
 });
 
-db.on('disconnected', function () {
-    console.log('Mongoose disconnected');
+db.on("disconnected", function() {
+  console.log("Mongoose disconnected");
 });
 
 //export the module
 module.exports = {
-    db,
-    mongoDBConnectinString,
-    host,
-    secretKey
+  db,
+  mongoDBConnectinString,
+  host,
+  secretKey
 };
