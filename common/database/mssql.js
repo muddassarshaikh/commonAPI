0ac5c154cf0358ec88ecf9dc3ad6c5c3294fd020
Muddassar S Shaikh <shaikh.muddassar8@gmail.com>
2020-04-28 16:30:33 +0530

    Added new database connection file for mssql

---
+++ common/database/mssql.js
@@ -0,0 +1,32 @@
+const sql = require('mssql');
+const config = require('../../config');
+
+const connection = new sql.ConnectionPool({
+  user: config.databaseUser,
+  password: config.databasePassword,
+  server: config.databaseHost,
+  database: config.databaseDatabaseName,
+  options: {
+    encrypt: true,
+  },
+});
+
+connection.connect();
+
+var con = new sql.Request(connection);
+
+sql.connect(config, function (err) {
+  if (err) {
+    console.log('Error connecting to Database');
+    return;
+  }
+  console.log(
+    'Connection established to Host - [' +
+      config.databaseHost +
+      '] DB - [' +
+      config.databaseDatabaseName +
+      ']'
+  );
+});
+
+module.exports = con;
